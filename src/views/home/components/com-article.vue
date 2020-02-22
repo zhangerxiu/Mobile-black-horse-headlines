6f20ab0a2a3cd327254902da8b538ea841b612cf
zhangerxiu <1714161982@qq.com>
2020-02-22 11:56:50 +0800

    完成真实文章列表，内容呈现

--- src/views/home/components/com-article.vue
+++ src/views/home/components/com-article.vue
@@ -16,17 +16,33 @@
               内容独占一行进行显示，使用其他div也可以
               title：设置单元格标题
       -->
-      <van-cell v-for="item in list" :key="item" :title="item"/>
+      <!-- <van-cell v-for="item in list" :key="item" :title="item"/> -->
+      <van-cell
+          v-for="item in articleList"
+          :key="item.art_id.toString()"
+          :title="item.title"/>
     </van-list>
      </van-pull-refresh>
   </div>
 </template>
 
 <script>
+// 导入获得文章的api函数
+import { apiArticleList } from '@/api/article.js'
 export default {
   name: 'com-article',
+  props: {
+  // 当前选中的频道id信息
+    channelID: {
+      type: Number,
+      required: true
+    }
+  },
   data () {
     return {
+      // 文章列表
+      articleList: [],
+      ts: Date.now(), // 时间戳参数，用于分页获取文章信息
       // 下拉刷新标志
       isLoading: false,
       // 上拉相关成员
@@ -36,8 +52,21 @@ export default {
       finished: false // 加载是否停止的标志，false可以继续加载，true瀑布流停止加载，如果后端没有数据可以提供了，就设置该项目为true即可
     }
   },
+  created () {
+  // 文章
+    this.getArticleList()
+  },
   methods: {
-
+    // 获得文章列表
+    async getArticleList () {
+      const result = await apiArticleList({ channel_id: this.channelID, timestamp: this.ts })
+      console.log(result)
+      // data接收文章数据
+      // this.articleList = result.results
+      // 把获得好的文章列表做return返回，
+      // 具体是给onLoad瀑布使用，在瀑布里边实现push追加
+      return result
+    },
     // 下拉刷新载入
     onRefresh () {
       setTimeout(() => {
@@ -47,24 +76,26 @@ export default {
       }, 1000)
     },
     // 上拉瀑布流加载执行的方法
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      // setTimeout设置延迟加载效果，有1s延迟
-      setTimeout(() => {
-        // 把数字信息追加给list数字
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
+    // 瀑布流加载执行的方法
+    async onLoad () {
+      // 1. 获得文章列表数据
+      //    注意：设置await，使得当前的axios异步进程变为同步的，先执行完，再执行后续代码
+      const articles = await this.getArticleList()
 
-        // 加载状态结束
-        this.loading = false // '加载中。。'动画清除
+      if (articles.results.length > 0) {
+        // 2. 把获得到的文章数据push追加给articleList成员
+        //    articles.results: 文章的数组对象集 [{art_id,title,aut_id,pubdate},{……},{……}]
+        //    ...articles.results：扩展运算  {art_id,title,aut_id,pubdate},{……},{……}
+        this.articleList.push(...articles.results)
+        // 更新时间戳
+        this.ts = articles.pre_timestamp // 使得继续请求，可以获得下页数据
+      } else {
+        // 4. 数据已经耗尽，就要停止瀑布了
+        this.finished = true // 停止瀑布流加载
+      }
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true // 停止瀑布流加载
-        }
-      }, 1000)
+      // 3. 清除上拉动画效果
+      this.loading = false // '加载中。。'动画清除
     }
   }
 }
